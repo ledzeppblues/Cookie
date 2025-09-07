4131798bcd6235c1be508e0497e288d1e43c7f05
ledzeppblues <59486160+ledzeppblues@users.noreply.github.com>
2025-09-07 21:19:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const modal = document.getElementById("modal");
+
+modal.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  modal.classList.add("img-only");
+  modal.innerHTML = "";
+  modal.innerHTML = `
+  <div class="img">
+    <img src="fade-stagger-circles.svg" alt="loading" width="120px">
+    <div>
+    <h2>Hold up, just selling your data</h2>
+    </div>
+  </div>
+`;
+
+  setTimeout(function () {
+    modal.innerHTML = `<div class="img">
+    <img src="fade-stagger-circles.svg" alt="loading" width="120px">
+    <div>
+    <h2>Making the sale</h2>
+    </div>
+  </div>`;
+  }, 1500);
+
+  setTimeout(function () {
+    modal.innerHTML = `<div class="img">
+    <img src="fade-stagger-circles.svg" alt="loading" width="120px">
+    <div>
+    <h2>This was a waste of time 😑</h2>
+    </div>
+  </div>`;
+  }, 3000);
+});
